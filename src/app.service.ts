3ccd35f2ea655630b7b3ba2ddf6303729a89b8d2
Atul Kadian <atul2143143@gmail.com>
2024-07-18 10:18:04 +0530

    feat: add build and lint workflow
    Signed-off-by: Atul Kadian <atul2143143@gmail.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const env = process.env.ENVIRONMENT;
     return 'Hello World!';
   }
 }
