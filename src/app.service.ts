be45e14df51f8441621b66a078cd9121a77439c7
Atul Kadian <atul2143143@gmail.com>
2024-07-18 10:20:01 +0530

    fix: remove unused var
    Signed-off-by: Atul Kadian <atul2143143@gmail.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const env = process.env.ENVIRONMENT;
     return 'Hello World!';
   }
 }
